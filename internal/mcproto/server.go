95b1032437c655deb812502472774151d2733cca
Sergi Vos <sergivb01@gmail.com>
2020-04-10 00:41:16 +0200

    changes idk

--- internal/mcproto/server.go
+++ internal/mcproto/server.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"time"
 
@@ -240,47 +239,3 @@ func (c *connectorImpl) findAndConnectBackend(ctx context.Context, frontendConn 
 	c.pumpConnections(ctx, frontendConn, backendConn)
 }
 
-func (c *connectorImpl) pumpConnections(ctx context.Context, frontendConn, backendConn net.Conn) {
-	//noinspection GoUnhandledErrorResult
-	defer backendConn.Close()
-
-	clientAddr := frontendConn.RemoteAddr()
-	defer c.log.Debug().
-		Str("client", clientAddr.String()).
-		Str("backendConn", backendConn.RemoteAddr().String()).
-		Msg("closing backend connection")
-
-	errors := make(chan error, 2)
-	go c.pumpFrames(backendConn, frontendConn, errors, "backend", "frontend", clientAddr)
-	go c.pumpFrames(frontendConn, backendConn, errors, "frontend", "backend", clientAddr)
-
-	select {
-	case err := <-errors:
-		if err != io.EOF {
-			c.log.Error().
-				Err(err).
-				Str("client", clientAddr.String()).
-				Str("backendConn", backendConn.RemoteAddr().String()).
-				Msg("error on connection relay")
-			log.Printf("error in conn relay: %s", err)
-		}
-
-	case <-ctx.Done():
-		c.log.Debug().Msg("received context cancellation")
-	}
-}
-
-func (c *connectorImpl) pumpFrames(incoming io.Reader, outgoing io.Writer, errors chan<- error, from, to string, clientAddr net.Addr) {
-	amount, err := io.Copy(outgoing, incoming)
-	c.log.Debug().
-		Str("client", clientAddr.String()).
-		Int64("amount", amount).
-		Msgf("finished relay %s->%s", from, to)
-
-	if err != nil {
-		errors <- err
-	} else {
-		// successful io.Copy return nil error, not EOF… to simulate that to trigger outer handling
-		errors <- io.EOF
-	}
-}
