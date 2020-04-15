41a184a5ce20bd4f4494d93a1684bb1e9ee9dbc8
Sergi Vos <sergivb01@gmail.com>
2020-04-15 04:50:22 +0200

    fix test

--- internal/proto/packetcodec_test.go
+++ internal/proto/packetcodec_test.go
@@ -1,20 +1,22 @@
-package packet
+package proto
 
 import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/sergivb01/mctunnel/internal/proto"
+	"github.com/sergivb01/mctunnel/internal/proto/packet"
 )
 
+// TODO: add Read method with a const byte slice of an example handshake
+
 func BenchmarkHandshake_Write(b *testing.B) {
-	h := &Handshake{
+	h := &packet.Handshake{
 		ProtocolVersion: 47,
 		ServerAddress:   "lunar.gg.tunnel.sergitest.dev",
 		ServerPort:      25565,
 		State:           1,
 	}
-	p := proto.NewPacketCodec()
+	p := NewPacketCodec()
 	for i := 0; i < b.N; i++ {
 		if err := p.EncodePacket(ioutil.Discard, h); err != nil {
 			b.Error(err)
